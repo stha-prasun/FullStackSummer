3ce7f05e172deff01572c8a046a166eb95548594
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-07-02 10:57:52 +0545

    Add User

--- Wk2/Tutorial/Backend/controller/userController.js
+++ Wk2/Tutorial/Backend/controller/userController.js
@@ -1,8 +1,35 @@
+import { Professional } from "../models/professionalSchema.js";
+
 export const test = async (req, res) => {
   try {
     return res.status(201).json({
       success: true,
       message: "Helloo",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addUser = async (req, res) => {
+  try {
+    const { name, category } = req.body;
+
+    if (!name || !category) {
+      return res.status(400).json({
+        message: "Email Already In Use!!",
+        success: false,
+      });
+    }
+
+    await Professional.create({
+      name,
+      category,
+    });
+
+    return res.status(201).json({
+      message: "User created successfully!!",
+      success: true,
     });
   } catch (error) {
     console.log(error);
--- Wk2/Tutorial/Backend/routes/userRoutes.js
+++ Wk2/Tutorial/Backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { test } from "../controller/userController.js";
+import { addUser, test } from "../controller/userController.js";
 
 const router = express.Router();
 
 router.route("/test").get(test);
+
+router.route("/add").post(addUser);
 
 export default router;
